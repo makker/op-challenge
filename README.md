1c54f1d8a256e65fe4c5b10fa4102d13c6143e87
Markus Ruottu <makkeruottu@gmail.com>
2018-02-25 18:36:38 +0200

    version for testing

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # op-challenge
 This was done as part of a job application
+
+## Browser support
+IE >= 9
+All all modern desktop and mobile browser, but no niche browsers like Opera Mini
+
+Semantic HMTL elements were not used to support IE.
+
+## Solutions
+
+### Text content
+As there was only contents provided for one of the 3 sections, I used the same contents for the two others as well.
+
+### Fonts
+The font weights in the numeric layout instructions were too much. For example based on the instructions main heading should have font weight 600, but in Chrome with font weight 500 the result looks mush more like the layout image.
+
+For other texts there was no weight instructed in the layouts. Chrome defaults to 400, but in looked too bold. 
+
+I ended up using 300 and 500, but normally I would've ofcourse consulted the designer which weight he/she ment to be used.
+
+### SVGs
+I decided to use the SVGs for the chevrons as the original asset bundle didn't include the Opuxicons font. That font is used in the OP site to produce the animated colorized chevron. 
+
+### Class names
+Classes and elements would be named differently if this the page was part of a larger system.
+
+### Dev features
+Js and CSS include some features that can be activated by adding '?dev' after the index.html url. Features were used in development and would be removed before deploy to prod.
+- CSS adds a layout tracing image as bg
+- Opens first item
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,54 @@
 
+
+function inactivateSection(section) {
+    var classes = section.className.split(" ");
+    classes.splice(classes.indexOf("open"), 1);
+    section.className = classes.join(" ");
+    
+    var container = section.getElementsByClassName("container-info")[0];
+    container.style.height = 0;
+}
+function inactivateSections(keep) {
+    var openedSections = document.querySelectorAll("section.open");
+
+    openedSections.forEach(function(section) {
+        if (section.className.indexOf("open") > -1) {
+            inactivateSection(section);
+        }
+    });
+}
+function activateSection(section) {
+    var alreadyOpen = (section.className.indexOf("open") > -1);
+    inactivateSections();
+
+    if (!alreadyOpen) {
+        // If not already open then add class open
+        var classes = (section.className.length === 0) ? [] : section.className.split(" ");
+        classes.push("open");
+        section.className = classes.join(" ");
+
+        // Set calculated height as a style to enable CSS transition
+        var container = section.getElementsByClassName("container-info")[0];
+        var wrapper = container.getElementsByClassName("wrapper-info")[0];
+        container.style.height = wrapper.clientHeight + "px";
+    }
+}
 function init() {
+    // set click handlers to section headers
+    var sectionHeaders = document.querySelectorAll("section > header");
+    sectionHeaders.forEach(function(header){
+        header.addEventListener("click", function(event){
+            var section = this.parentNode;
+            activateSection(section);
+        }, false);
+    });
+
+    // set height=0 to section body containers
+    var sectionBodys = document.querySelectorAll("section > .container-info");
+    sectionBodys.forEach(function(sectionBody){
+        sectionBody.style.height = 0;
+    });
+
     // If dev url param then add class dev to body to enable some dev enhancements
     // This feature should be removed before deploying to production
     if (window.location.href.indexOf("?dev") > -1) {
@@ -8,7 +57,24 @@ function init() {
         var classes = (body.className.length === 0) ? [] : body.className.split(" ");
         classes.push("dev");
         body.className = classes.join(" ");
+    
+        // Open a section for inspection
+        var sectionToOpen = document.querySelectorAll("section")[1];
+        console.log("sectionToOpen: ", sectionToOpen);
+        activateSection(sectionToOpen);
     }
 };
 
 document.addEventListener('DOMContentLoaded', init, false);
+
+/* Polyfill for NodeList.forEach from 
+ * https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+ */
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
